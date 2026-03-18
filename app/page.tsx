f6bdb0dd1bb2ea172927798db13954d8744f387c
maudonestas <maud.onestas@gmail.com>
2026-03-18 17:34:47 +1000

    Delete app directory

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: "40px", fontFamily: "Arial" }}>
-      <h1>Christine Béglet</h1>
-      <p>Site en cours de création.</p>
-    </main>
-  );
-}
