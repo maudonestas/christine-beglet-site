99dde117a9c2f174bed32783ee6a4626acea4ddf
maudonestas <maud.onestas@gmail.com>
2026-03-18 17:24:01 +1000

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1>Christine Béglet</h1>
+      <p>Site en cours de création.</p>
+    </main>
+  );
+}
