1e8094fa31118e9ebf1845e5ec1cc0cdb27ff423
maudonestas <maud.onestas@gmail.com>
2026-04-11 14:14:54 +1000

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -4,7 +4,7 @@ import type { CSSProperties } from "react";
 export default function ContactPage() {
   return (
     <main style={styles.main}>
-      <Header />
+
 
       <section style={styles.section}>
         <div style={styles.container}>
