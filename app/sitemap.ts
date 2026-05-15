5eaeca80c7ab859964d0a91876caa88b51f26541
maudonestas <maud.onestas@gmail.com>
2026-05-16 09:57:34 +1000

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -81,12 +81,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.8,
     },
     {
-      url: `${baseUrl}/en/home`,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
-    },
-    {
       url: `${baseUrl}/en/works/large-formats`,
       lastModified: new Date(),
       changeFrequency: "monthly",
